6e47396457deaaeadff4c0aa19829e34bf044487
wander0000 <yeonjuseo712@gmail.com>
2024-07-17 14:13:52 +0900

    mybatis commit

---
+++ src/main/resources/static/js/js/company_InfoUpdate.js
@@ -0,0 +1,59 @@
+$(document).ready(function()
+{
+    // /*
+    //     2024-06-25 이재원 
+    //     메뉴 눌렀을 때 메뉴 활성화 : active
+    // */
+    // $('.navMenu ul li').click(function(){
+    //     $(this).addClass('active');
+    //     $('.navMenu ul li').not(this).removeClass('active');
+    // });
+
+    
+    // /*
+    //     2024-06-25 이재원 
+    //     글자수 제한 + 넘는건 ...처리
+    // */
+    // $('.cardConBottom > .title').each(function()
+    // {
+    //     var length = 21; //표시할 글자 수 정하기
+    
+    //     $(this).each(function()
+    //     {
+
+    //         if($(this).text().length >= length)
+    //         {
+    //             $(this).text($(this).text().substr(0, length) + '...');	//지정한 글자수 이후 표시할 텍스트 '...'
+    //         }
+    //     });
+
+    // });
+
+    // /*
+    //     2024-06-25 이재원 
+    //     탭 메뉴 : 전체 , 포지션 제안, 관심기업
+    // */
+
+    // $('.tabCon.All').click(function(){
+    //     $('.cardConWrap').css({"display":"none"});
+    //     $('.cardConWrap.All').css({"display":"flex"});
+    // });
+
+    // $('.tabCon.proposal').click(function(){
+    //     $('.cardConWrap').css({"display":"none"});
+    //     $('.cardConWrap.proposal').css({"display":"flex"});
+    // });
+
+    // $('.tabCon.post').click(function(){
+    //     $('.cardConWrap').css({"display":"none"});
+    //     $('.cardConWrap.post').css({"display":"flex"});
+    // });
+
+    
+
+
+});
+    
+    
+    
+
